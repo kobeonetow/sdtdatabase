518a3c9ddbeaa08da888cc1eeb035e39a13e24c7
= <=>
2013-08-06 19:07:14 +0800

    add more fields and tables

--- vehicleprofile.sql
+++ vehicleprofile.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS vehicleprofile;
 CREATE TABLE vehicleprofile(
 id SERIAL PRIMARY KEY,
 vid VARCHAR(16) NOT NULL,
+selfid VARCHAR(8),
 company VARCHAR(64),
 companyaddr VARCHAR(128),
 datejoin DATE,
@@ -15,6 +16,7 @@ vehicleprice VARCHAR(10),
 subprice VARCHAR(8),
 dateproduction DATE,
 productionaddr VARCHAR(128),
+productioncode VARCHAR(64),
 madein VARCHAR(8),
 vtype VARCHAR(8),
 vlevel VARCHAR(8),
@@ -99,4 +101,61 @@ creator INT,
 FOREIGN KEY (vid) REFERENCES vehicleprofile(id),
 FOREIGN KEY (fid) REFERENCES vehiclefiles(id),
 FOREIGN KEY (creator) REFERENCES account(id)
+);
+
+DROP TABLE IF EXISTS vehicleteam;
+CREATE TABLE vehicleteam(
+id SERIAL PRIMARY KEY,
+name VARCHAR(16),
+description VARCHAR(256)
+);
+
+DROP TABLE IF EXISTS vehiclelane;
+CREATE TABLE vehiclelane(
+id SERIAL PRIMARY KEY,
+num VARCHAR(8),
+detail VARCHAR(256)
+);
+
+DROP TABLE IF EXISTS vehicleteamleader;
+CREATE TABLE vehicleteamleader(
+id SERIAL PRIMARY KEY,
+teamid INT NOT NULL,
+employeeid INT NOT NULL,
+FOREIGN KEY (teamid) REFERENCES vehicleteam(id),
+FOREIGN KEY (employeeid) REFERENCES employee(id)
+);
+
+DROP TABLE IF EXISTS vehicleteammember;
+CREATE TABLE vehicleteammember(
+id SERIAL PRIMARY KEY,
+vid INT NOT NULL,
+teamid INT NOT NULL,
+FOREIGN KEY (vid) REFERENCES vehicleprofile(id),
+FOREIGN KEY (teamid) REFERENCES vehicleteam(id)
+);
+
+
+DROP TABLE IF EXISTS vehiclelanemapper;
+CREATE TABLE vehiclelanemapper(
+id SERIAL PRIMARY KEY,
+vid INT NOT NULL,
+laneid INT NOT NULL,
+FOREIGN KEY (vid) REFERENCES vehicleprofile(id),
+FOREIGN KEY (laneid) REFERENCES vehiclelane(id)
+);
+
+DROP TABLE IF EXISTS vehicledriver;
+CREATE TABLE vehicledriver(
+id SERIAL PRIMARY KEY,
+vid INT NOT NULL,
+laneid INT NOT NULL,
+driver1 INT NOT NULL,
+driver2 INT NOT NULL,
+remark VARCHAR(256),
+ddate DATE,
+FOREIGN KEY (vid) REFERENCES vehicleprofile(id),
+FOREIGN KEY (laneid) REFERENCES vehiclelane(id),
+FOREIGN KEY (driver1) REFERENCES employee(id),
+FOREIGN KEY (driver2) REFERENCES employee(id)
 );
